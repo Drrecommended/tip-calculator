2d03fd8c4b70e553e324b85847bd14998d78c42c
Tyler French <tylerfrench89@gmail.com>
2021-09-26 14:19:28 -0700

    more styling to mobile view

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fivePercent = document.getElementById('five').innerText
 const customPercent = document.getElementById('custom').value
 console.log(customPercent)
+
+
 //Check that key press is a number
 const checkForNum = (e) => {
   let char = String.fromCharCode(e.which);
