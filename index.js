5532592cb4e769d11e3b4c14b7d9d3108ea721fb
Tyler French <tylerfrench89@gmail.com>
2021-10-04 18:40:34 -0700

    added vendor file to sort code better

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const fivePercent = document.getElementById('five').innerText
-const customPercent = document.getElementById('custom').value
-console.log(customPercent)
+
 
 
 //Check that key press is a number
@@ -8,7 +6,13 @@ const checkForNum = (e) => {
   let char = String.fromCharCode(e.which);
   if (!/[0-9]/.test(char)) {
     e.preventDefault();
-  } 
+  }
 };
 
+const getBillTotal = () => {
+  console.log(billTotal.value)
+}
 
+
+
+// fivePercent.addEventListener("click", grabPercentage);
---
+++ vendor.js
@@ -0,0 +1,14 @@
+const billTotal = document.getElementById("bill-amount");
+const numOfPeople = document.getElementById("people-count");
+const customPercent = document.getElementById("custom");
+
+const fivePercent = document.getElementById("five");
+const tenPercent = document.getElementById("ten");
+const fifteenPercent = document.getElementById("fifteen");
+const twentyFivePercent = document.getElementById("twenty-five");
+const fiftyPercent = document.getElementById("fifty");
+
+const tipAmount = document.getElementById("tip-amount");
+const total = document.getElementById("total-amount");
+
+
