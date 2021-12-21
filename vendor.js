cb99062b814c0bdc1093d92b0bdaf63f25683a89
Tyler French <tylerfrench89@gmail.com>
2021-12-20 19:32:11 -0800

    added all handlers on inputs and buttons starting over

--- vendor.js
+++ vendor.js
@@ -1,54 +1,37 @@
+const bill = document.getElementById("bill");
+const buttons = document.querySelectorAll("#percent-buttons");
+const custumPercent = document.getElementById("custom-percent");
+const personCount = document.getElementById("person-count");
+const tipPerPerson = document.getElementById("tip-per-person");
+const totalPerperson = document.getElementById("total-per-person");
+const resetButton = document.getElementById("reset-button");
+
+const setBillValue = () => {
+  return console.log(bill.value);
+};
+
+const clickHandler = (e) => {
+  console.log(e.target);
+  
+};
+
+const setCustomPercent = () => {
+  console.log(custumPercent.value);
+};
+
+const setPersonCount = () => {
+  return console.log(personCount.value);
+};
+
+const resetHandler = () => {
+    console.log('clicked')
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const tipTotal = document.getElementById("tip-amount");
-// const total = document.getElementById("total-amount");
-
-// //output tip per person
-// const outputTipResult = (tipAmount) => {
-//   tipTotal.textContent = tipAmount;
-// };
-// //output bill total per person
-// const outputBillResult = (billAmount) => {
-//   console.log(billAmount)
-//   total.textContent = billAmount;
-// };
-
-// (function(el){el.value=parseFloat(el.value).toFixed(2);})(this)
+bill.addEventListener("input", setBillValue);
+buttons.forEach((button) => {
+  button.addEventListener("click", clickHandler);
+});
+custumPercent.addEventListener("input", setCustomPercent);
+personCount.addEventListener('input', setPersonCount)
+resetButton.addEventListener('click', resetHandler)
