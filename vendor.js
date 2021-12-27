1cb8c3cc3bb3c4506d8fd63c25170872867eee7e
Tyler French <tylerfrench89@gmail.com>
2021-12-26 22:08:15 -0800

    warning now around input

--- vendor.js
+++ vendor.js
@@ -6,6 +6,8 @@ const tipPerPerson = document.getElementById("tip-per-person");
 const totalPerperson = document.getElementById("total-per-person");
 const resetButton = document.getElementById("reset-button");
 
+const DEFAULT_PERSON_COUNT = 1;
+
 const setBillValue = () => {
   return console.log(bill.value);
 };
@@ -14,17 +16,31 @@ const clickHandler = (e) => {
   //   let clickedButtonValue = e.target.attributes["data-value"].value;
   let selectedButton = e.target;
   buttons.forEach((button) => {
-    button.classList.remove('active')
-    if(button.value == selectedButton.value) {
-        selectedButton.classList.add('active')
+    button.classList.remove("active");
+    if (button.value == selectedButton.value) {
+      selectedButton.classList.add("active");
     }
-
-    
   });
 
-  if (setPersonCount >= 0) {
-    console.log("this is good");
+  //   personCount.value = ""
+
+  calculateResult(selectedButton);
+};
+
+const calculateResult = (b) => {
+  console.log(b.attributes["data-value"].value);
+  //  totalPerperson.innerHTML = `$${}`
+
+  let currentCount = setPersonCount();
+  if (currentCount) {
+    document.querySelector(".warning").style.visibility = "hidden";
+  } else if (!currentCount || currentCount == 0) {
+    personCount.classList.add("border-warning")
+    // personCount.style.border = "1px solid #c9775d"
+    document.querySelector(".warning").style.visibility = "visible";
+    currentCount = DEFAULT_PERSON_COUNT;
   }
+  console.log("her");
 };
 
 const setCustomPercent = () => {
